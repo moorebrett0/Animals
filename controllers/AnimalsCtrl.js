2598bd7332db9d387bccae5c9c8f70ccf29fb3c1
Conor Baumgart & Brett Moore <student@epicodus.com>
2015-04-21 15:23:02 -0700

    Finish project

--- controllers/AnimalsCtrl.js
+++ controllers/AnimalsCtrl.js
@@ -1,4 +1,13 @@
 animalShelter.controller('AnimalsCtrl', function AnimalCtrl($scope, AnimalFactory) {
     $scope.animals = AnimalFactory.animals;
     $scope.AnimalFactory = AnimalFactory;
+    $scope.addAnimal = function() {
+        var name = $scope.animalName;
+        var species = $scope.animalSpecies;
+        var vaccinated = $scope.vaccinated;
+        AnimalFactory.addAnimal(name, species, vaccinated);
+        $scope.animalName = null;
+        $scope.animalSpecies = null;
+        $scope.vaccinated = null;
+    }
 });
--- service/AnimalFactory.js
+++ service/AnimalFactory.js
@@ -1,16 +1,13 @@
 animalShelter.factory('AnimalFactory', function AnimalFactory() {
     var factory = {};
     factory.animals = [
-        { name: "Ted", species: "Cat", vaccinated: true }
+        { name: "Ted", species: "Cat", vaccinated: true, adopted: false }
     ];
 
-    factory.addAnimal = function() {
-        var animal = { name: factory.animalName, species: factory.animalSpecies, vaccinated: factory.vaccinated};
+    factory.addAnimal = function(name, species, vaccinated) {
+        var animal = { name: name, species: species, vaccinated: vaccinated, adopted: false };
         factory.animals.push(animal);
-        debugger;
-        factory.animalName = null;
-        factory.animalSpecies = null;
-        factory.vaccinated = false;
     };
+
     return factory;
 });
